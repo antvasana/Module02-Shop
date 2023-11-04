1d90832c90d79143beaa4e8aad6af3ad08863102
antvasana <tgurieva04@gmail.com>
2023-11-04 17:38:23 -0400

    Add slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import 'normalize.css'
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss';
-// import './index.html';
+
+var swiper = new Swiper(".offerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // slidesPerGroup: 3,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation, Pagination],
+});
